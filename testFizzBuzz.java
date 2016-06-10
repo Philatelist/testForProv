7b3d44b23f3f2db1a7733cc02bf2e40c59dcc333
Alexander Usov <a.usoff@gmail.com>
2016-06-10 19:00:27 +0300

    New way solutions

--- testFizzBuzz.java
+++ testFizzBuzz.java
@@ -5,14 +5,7 @@ public class testFizzBuzz {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0) {
-                System.out.print("Fizz");
-            }
-            if (i % 5 == 0) {
-                System.out.print("Buzz");
-            } else if (i % 5 != 0 && i % 3 != 0) {
-                System.out.print(i);
-            }
+            System.out.println((i % 15 == 0) ? "FizzBuzz" : ((i % 5 == 0) ? "Buzz" : ((i % 3 == 0) ? "Fizz" : i)));
         }
     }
 }
